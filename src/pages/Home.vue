3a0fd6ca1a03e5335a0e1be2daf8e2b787060228
Jack Branthwaite <jack@formfunfunction.com>
2021-08-23 15:53:19 +1200

    Styling and junk removed from vite app

---
+++ src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Basic setup with styling and nonsense removed</h1>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+export default defineComponent({
+    name: 'Home',
+    props: {
+        msg: {
+            type: String,
+            required: true,
+        },
+    },
+    setup: () => {
+        const count = ref(0)
+        return { count }
+    },
+})
+</script>
